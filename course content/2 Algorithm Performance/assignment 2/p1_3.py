9307ac6b35a74bd5f32b104f301e9237b4b092bd
zlrkw11 <ray040406@gmail.com>
2024-07-26 17:08:47 +1200

    fix

--- course content/2 Algorithm Performance/assignment 2/p1_3.py
+++ course content/2 Algorithm Performance/assignment 2/p1_3.py
@@ -1,8 +1,10 @@
-a = [1,2,3]
+a = [1,2,3,4]
 n = len(a)
-count = 0
+print(n)
+
 def sifynia(n):
-    for i in range(1, n/2+1):
+    count = 0
+    for i in range(1, int(n/2)+1):
         a[2*i-1] = 3*i
         a[2*i] = i-1
         count+=1
@@ -11,4 +13,5 @@ def sifynia(n):
             return 
         count+=1
     print(count)
-    
+
+sifynia(n)
