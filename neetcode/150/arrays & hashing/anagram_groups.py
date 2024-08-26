d76f7a3a3b3d2a5d2f2d6c8abaa36218c209477d
ZL <ray040406@gmail.com>
2024-08-26 22:38:04 +1200

    anagrams

--- neetcode/150/arrays & hashing/anagram_groups.py
+++ neetcode/150/arrays & hashing/anagram_groups.py
@@ -1,8 +1,23 @@
+# given a list of strings, check if they are anagrams
+
+# 2 strings are anagrams if we SORT them (tan) (nat) -> (ant)
+# time: O(m * nlogn)
+
+# instead, count the number of alphabets each word has, record with a hashmap
+# [1e, 1a, 1t] = ['eat'], ['ate'], ['tea']
+# O(m * n * 26) = O(m * n)
+
+from sklearn.base import defaultdict
+
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        n = []
-        for i in range(len(strs)):
-            for j in range(i+1, len(strs)):
-                if [k for k in strs[i]].sort() == [h for h in strs[j]].sort():
-                    n += [strs[i], strs[j]]
-        return n
+        res = defaultdict(list) # mapping the charCount to list of anagrams
+        for s in strs:
+            count = [0] * 26 # a..z
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1 # find the c using ascii, increment
+            
+            res[tuple(count)].append(s) # list cannot be keys
+        return res.values()
