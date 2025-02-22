9653a29666ac965a82ee155be7458075f09affce
ZL <ray040406@gmail.com>
2025-02-22 23:01:59 +1300

    wrong try 2

--- leetcode/easy/can_place_flowers.py
+++ leetcode/easy/can_place_flowers.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def canPlaceFlowers(self, flowerbed, n):
     
-        if(len(flowerbed) % 2 == n % 2):
+        l = len([flower for flower in flowerbed if flower == 0])
+        if (l == n * 2 + 1):
             return True
-        return False 
+        return False
         
         
 solution = Solution()
+print(solution.canPlaceFlowers([1,0,0,0,0,1], 1))
 print(solution.canPlaceFlowers([1,0,0,0,1], 1))
