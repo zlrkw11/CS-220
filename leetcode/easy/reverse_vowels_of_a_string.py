e6cadc28dc33bc4ac3d3562ca0ce995eb08b419b
ZL <ray040406@gmail.com>
2025-02-23 17:03:51 +1300

    double pointer

---
+++ leetcode/easy/reverse_vowels_of_a_string.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowels = "aeiouAEIOU"
+        i, j = 0, len(s) - 1
+        cs = list(s)
+        while i < j:
+            while i < j and cs[i] not in vowels:
+                i += 1
+            while i < j and cs[j] not in vowels:
+                j -= 1
+            if i < j:
+                cs[i], cs[j] = cs[j], cs[i]
+                i, j = i + 1, j - 1
+        return "".join(cs)
+        
+        
+        
+solution = Solution()
+print(solution.reverseVowels("LeetCode"))
