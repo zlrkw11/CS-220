d30c96597dd30252cd4aedab9e476e5774eb0d8a
ZL <ray040406@gmail.com>
2025-03-05 19:10:52 +1300

    167

---
+++ leetcode/medium/three_sum.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        return True
+        
+solution = Solution()
+print(solution.threeSum())
