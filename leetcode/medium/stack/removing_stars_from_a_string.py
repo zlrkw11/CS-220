6e1ed396cf274057b2ad5b09f032bd207073c28a
ZL <ray040406@gmail.com>
2025-03-07 18:47:03 +1300

    stack

---
+++ leetcode/medium/stack/removing_stars_from_a_string.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def removeStars(self, s: str) -> str:
+        arr = []
+        
+        for i in range(len(s)):
+            pass
+        
+        return str(arr)
+# "*"
+
+solution = Solution()
+print(solution.removeStars("leet**cod*e"))
