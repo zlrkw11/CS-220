c6e873bebd7ff6517b7c3054d28952e7528ce253
ZL <ray040406@gmail.com>
2025-03-09 01:20:24 +1300

    xx

---
+++ leetcode/medium/stack/asteroid_collision.py
@@ -0,0 +1,10 @@
+from typing import List 
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+       
+        return 0
+
+solution = Solution()
+print(solution.asteroidCollision([5,10,-5]))
+print(solution.asteroidCollision([8,-8]))
+print(solution.asteroidCollision([10,2,-5]))
