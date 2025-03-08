975146c548d3f20d268f4a1f7efab2ec300b655f
ZL <ray040406@gmail.com>
2025-03-09 12:31:38 +1300

    q20

--- leetcode/easy/valid_parentheses.py
+++ leetcode/easy/valid_parentheses.py
@@ -2,13 +2,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        d = {"()","[]","{}"}
         for c in s:
-        
-            stack.append(c)
-            
-            stack.pop()
-        return len(stack)==0
+            if c in "([{":
+                stack.append(c)
+            elif not stack or c + stack.pop() not in d:
+                return False
+        return not stack
     
 
 solution = Solution()
-print(solution.isValid("([{}])"))
+print(solution.isValid("[][][]"))
