07679ebb833e71b32425f26ba71384c1bf1f27ff
ZL <ray040406@gmail.com>
2025-03-09 16:19:17 +1300

    x

--- leetcode/easy/binary_search.py
+++ leetcode/easy/binary_search.py
@@ -3,14 +3,14 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         low, high = 0, len(nums)-1
         while low <= high:
-            mid = low + high // 2
-            if mid == target:
+            mid = low + (high-low) // 2
+            if nums[mid] == target:
                 return mid
             elif mid < target:
                 low = mid+1
             else:
                 high = mid-1            
-        return False
+        return -1
 
 solution = Solution()
-print(solution.search([]))
+print(solution.search([-1,0,2,4,6,8], 4))
