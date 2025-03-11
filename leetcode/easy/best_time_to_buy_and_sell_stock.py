289d7876d28b162320827593a00039a1bb4b9fa0
ZL <ray040406@gmail.com>
2025-03-11 13:52:15 +1300

    x

--- leetcode/easy/best_time_to_buy_and_sell_stock.py
+++ leetcode/easy/best_time_to_buy_and_sell_stock.py
@@ -2,7 +2,7 @@ from math import inf
 from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ans, low = 0, inf
+        ans, low = 0, float('inf')
         for price in prices:
             ans = max(ans, price-low)
             low = min(price, low)
